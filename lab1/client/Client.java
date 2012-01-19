bc41a7fd560098fefa0afb91bf20ec454fac63f5
Emil Arfvidsson <emil@arfvidsson.com>
2012-01-19 13:20:50 +0100

    Client and server now working

--- lab1/client/Client.java
+++ lab1/client/Client.java
@@ -1,9 +1,55 @@
-
+import java.net.*;
+import java.io.*;
 
 class Client {
 	
 	public static void main(String[] args) {
+		String address = args[0];
+		int port = 0;
+		try {
+			port = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
+		try {
+			// connect to server
+			System.out.println("connecting...");
+			final Socket socket = new Socket(address, port);
+			System.out.println("connected");
+			// thread to read input from user
+			Runnable readThread = new Runnable() {				
+				public void run() {
+					// read input from socket
+					try {
+						while (true) {
+							InputStream is = socket.getInputStream();
+							while (true) {
+								char c = (char) is.read();
+								System.out.print(c);
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			new Thread(readThread).start();
+
+			try {
+				while (true) {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+					String line = reader.readLine();
+					OutputStream os = socket.getOutputStream();
+					os.write(line.getBytes());
+					os.flush();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
--- lab1/server/Server.java
+++ lab1/server/Server.java
@@ -10,20 +10,25 @@ class Server {
 		
 		while (true) {
 			// wait for connection
+			System.out.println("Waiting for connection...");
 			final Socket socket = ss.accept();
+			System.out.println("Accepted connection.");
 			clientSockets.add(socket);
 			
 			// spawn thread to handle communication with client
-			Runnable clientThread = new Runnable() {
+			Runnable clientRunnable = new Runnable() {
 				public void run() {
+					System.out.println("Spawned");
 					try {
 						while (true) {
 							// read from socket
 							InputStream is = socket.getInputStream();
 							ByteArrayOutputStream baos = new ByteArrayOutputStream();
-							while (is.available() > 0) {
+							while (baos.size() == 0 || is.available() > 0) {
 							 	baos.write(is.read());
 							}
+							
+							System.out.println("Received: " + baos.toString());
 						
 							// write received data to all chat clients
 							synchronized (clientSockets) {
@@ -34,17 +39,21 @@ class Server {
 							}
 						}
 					} catch (IOException e) {
+						e.printStackTrace();
 						// client probably shut down, or some other read error occurred
 						// close socket and remove it
 						try {
 							socket.close();
-						} catch (IOException e1) {}
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
 						synchronized (clientSockets) {
 							clientSockets.remove(socket);
 						}
 					}
 				}
 			};
+			new Thread(clientRunnable).start();
 		}
 	}
 }
