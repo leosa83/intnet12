6a665f3840e9897ec8acbad7da9083edc1c1aa8b
Emil Arfvidsson <emil@appr.se>
2012-02-13 13:00:02 +0100

    Add android-chat

---
+++ lab6/android-chat/src/foo/robertemilpaketet/ChatActivity.java
@@ -0,0 +1,13 @@
+package foo.robertemilpaketet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
